696569aaf75cd39e730434efbbc3e37f863c07b5
JiWoo <didwldn97@naver.com>
2024-05-27 17:16:35 +0900

    로깅알아보기

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,22 +1,31 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j //log를 선언해주는 대신 사용(LomBok이 제공해주는 어노테이션)
 @RestController
 public class LogTestController {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+//    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/log-test")
     public String logTest() {
+
         String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace(" info log={}", name);
+        log.debug(" info log={}", name);
         log.info(" info log={}", name);
+        log.warn(" info log={}", name);
+        log.error(" info log={}", name);
 
         return "ok";
-
     }
 
 
